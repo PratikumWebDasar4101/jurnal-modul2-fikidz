7a6fb603bf2867e2f4909fb6dbbfbe0097793293
Fiki Dzulfiqar <33850678+fikidz@users.noreply.github.com>
2018-09-10 10:00:35 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,31 @@
 <?php 
-//Tuliskan kode anda di bawah ini
+error_reporting(0);
+$angka = $_POST['angka'];
+function pertama($x, $y){
+	if ($x > 0) {
+		if ($y > 0) {
+			echo "* ";
+			$y--;
+			pertama($x, $y);
+		}
+		else{
+			echo "<br>";
+			$x--;
+			pertama($x, $x);
+		}
+	}
+}
+pertama ($angka, $angka);
+
+echo "======================================================<br>";
+
+for($d=1;$d<=$angka;$d++)
+    {
+        if($d % 2 == 0){
+        	echo "$d = Bilangan Genap<br>";
+        }else{
+        	echo "$d = Bilangan Ganjil<br>";
+        }
+    }
 
 ?>
